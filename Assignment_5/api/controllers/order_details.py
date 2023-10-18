367496b4f0b6d663ea403de99fd734858a294830
Aaron Crandall <acranda1@uncc.edu>
2023-10-18 09:12:18 -0400

    Feature: moved the controllers to the right folder

--- Assignment_5/api/controllers/order_details.py
+++ Assignment_5/api/controllers/order_details.py
@@ -1,51 +1,50 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
-from ..models import models, schemas
+from Assignment_5.api.models import models, schemas
 
 
-def create(db: Session, resources):
+def create(db: Session, order_details):
     # Create a new instance of the Order model with the provided data
-    db_resources = models.Resource(
-        item=resources.item,
-        amount = resources.amount
+    db_order_details = models.OrderDetail(
+        amount=order_details.amount
 
     )
     # Add the newly created Order object to the database session
-    db.add(db_resources)
+    db.add(db_order_details)
     # Commit the changes to the database
     db.commit()
     # Refresh the Order object to ensure it reflects the current state in the database
-    db.refresh(db_resources)
+    db.refresh(db_order_details)
     # Return the newly created Order object
-    return db_resources
+    return db_order_details
 
 
 def read_all(db: Session):
-    return db.query(models.Resource).all()
+    return db.query(models.OrderDetail).all()
 
 
-def read_one(db: Session, resources_id):
-    return db.query(models.Resource).filter(models.Resource.id == resources_id).first()
+def read_one(db: Session, order_details_id):
+    return db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id).first()
 
 
-def update(db: Session, resources_id, resources):
+def update(db: Session, order_details_id, order_details):
     # Query the database for the specific order to update
-    db_resources = db.query(models.Resource).filter(models.Resource.id == resources_id)
+    db_order_details = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id)
     # Extract the update data from the provided 'order' object
-    update_data = resources.dict(exclude_unset=True)
+    update_data = order_details.dict(exclude_unset=True)
     # Update the database record with the new data, without synchronizing the session
-    db_resources.update(update_data, synchronize_session=False)
+    db_order_details.update(update_data, synchronize_session=False)
     # Commit the changes to the database
     db.commit()
     # Return the updated order record
-    return db_resources.first()
+    return db_order_details.first()
 
 
-def delete(db: Session, resources_id):
+def delete(db: Session, order_details_id):
     # Query the database for the specific order to delete
-    db_resources = db.query(models.Resource).filter(models.Resource.id == resources_id)
+    db_order_details = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id)
     # Delete the database record without synchronizing the session
-    db_resources.delete(synchronize_session=False)
+    db_order_details.delete(synchronize_session=False)
     # Commit the changes to the database
     db.commit()
     # Return a response with a status code indicating success (204 No Content)
--- Assignment_5/api/controllers/resources.py
+++ Assignment_5/api/controllers/resources.py
@@ -1,50 +1,51 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
-from ..models import models, schemas
+from Assignment_5.api.models import models, schemas
 
 
-def create(db: Session, order_details):
+def create(db: Session, resources):
     # Create a new instance of the Order model with the provided data
-    db_order_details = models.OrderDetail(
-        amount=order_details.amount
+    db_resources = models.Resource(
+        item=resources.item,
+        amount = resources.amount
 
     )
     # Add the newly created Order object to the database session
-    db.add(db_order_details)
+    db.add(db_resources)
     # Commit the changes to the database
     db.commit()
     # Refresh the Order object to ensure it reflects the current state in the database
-    db.refresh(db_order_details)
+    db.refresh(db_resources)
     # Return the newly created Order object
-    return db_order_details
+    return db_resources
 
 
 def read_all(db: Session):
-    return db.query(models.OrderDetail).all()
+    return db.query(models.Resource).all()
 
 
-def read_one(db: Session, order_details_id):
-    return db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id).first()
+def read_one(db: Session, resources_id):
+    return db.query(models.Resource).filter(models.Resource.id == resources_id).first()
 
 
-def update(db: Session, order_details_id, order_details):
+def update(db: Session, resources_id, resources):
     # Query the database for the specific order to update
-    db_order_details = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id)
+    db_resources = db.query(models.Resource).filter(models.Resource.id == resources_id)
     # Extract the update data from the provided 'order' object
-    update_data = order_details.dict(exclude_unset=True)
+    update_data = resources.dict(exclude_unset=True)
     # Update the database record with the new data, without synchronizing the session
-    db_order_details.update(update_data, synchronize_session=False)
+    db_resources.update(update_data, synchronize_session=False)
     # Commit the changes to the database
     db.commit()
     # Return the updated order record
-    return db_order_details.first()
+    return db_resources.first()
 
 
-def delete(db: Session, order_details_id):
+def delete(db: Session, resources_id):
     # Query the database for the specific order to delete
-    db_order_details = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_details_id)
+    db_resources = db.query(models.Resource).filter(models.Resource.id == resources_id)
     # Delete the database record without synchronizing the session
-    db_order_details.delete(synchronize_session=False)
+    db_resources.delete(synchronize_session=False)
     # Commit the changes to the database
     db.commit()
     # Return a response with a status code indicating success (204 No Content)
